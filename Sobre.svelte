e12ddc7d08c44e5e73bb83040a1757632162a749
IsaacLuiz88 <113178368+IsaacLuiz88@users.noreply.github.com>
2023-01-18 14:48:27 -0300

    Create Sobre.svelte
    o sobre do jogo, com as nossas informações

---
+++ Sobre.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/sobre.css">
+</svelte:head>
+
+<script>
+	import VoltarMenu from './VoltarMenu.svelte'
+</script>
+
+<h1>Sobre</h1>
+
+<img src="images/logoIFPE.png" alt="logo IPFE"/>
+
+<p>
+		Isaac Luiz - <a href="https://github.com/IsaacLuiz88" target="_blank">ilvp@discente.ifpe.edu.br</a>
+</p>
+<p>
+	Jennifer Fonseca - <a href="https://github.com/jenni101101" target="_blank">jfs57@discente.ifpe.edu.br</a>
+</p>
+<p>
+	Gabriella Batista - <a href="https://github.com/gabriellaBatista" target="_blank">gbs3@discente.ifpe.edu.br</a>
+</p>
+<p>
+	Bruno Stelzer - <a href="https://github.com/BrunoSTZ" target="_blank"> allan.lima@discente.ifpe.edu.br</a>
+</p>
+<p>
+	Jonatas Cândido - <a href="https://github.com/jenni101101" target="_blank">jca3@discente.ifpe.edu.br</a>
+</p>
+<VoltarMenu/>
